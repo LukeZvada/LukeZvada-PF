1b02b911ae376cbf4f5c2e712467a16ca3235f1b
LukeZvada <zvadamusic@gmail.com>
2020-12-10 20:19:59 -0500

    Updates

---
+++ precache-manifest.04329c54d75f5a77ff74db912f3ef10c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b100ac0667e983c63877258931449ed",
+    "url": "/lukezvada-pf/index.html"
+  },
+  {
+    "revision": "2ed7124bce05d5a06f96",
+    "url": "/lukezvada-pf/static/css/2.062ae2fb.chunk.css"
+  },
+  {
+    "revision": "1258c95e6ff0e3552d24",
+    "url": "/lukezvada-pf/static/css/main.cef2241c.chunk.css"
+  },
+  {
+    "revision": "2ed7124bce05d5a06f96",
+    "url": "/lukezvada-pf/static/js/2.a94cc1c1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/lukezvada-pf/static/js/2.a94cc1c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1258c95e6ff0e3552d24",
+    "url": "/lukezvada-pf/static/js/main.a4ba2c81.chunk.js"
+  },
+  {
+    "revision": "55511caf7ab7ce4fb468",
+    "url": "/lukezvada-pf/static/js/runtime-main.bac17900.js"
+  },
+  {
+    "revision": "dddf28fe8c3cc12111c8068f1d1db134",
+    "url": "/lukezvada-pf/static/media/AXDlogo.dddf28fe.png"
+  },
+  {
+    "revision": "90264d25bdc8c70d30073fd73fe3b042",
+    "url": "/lukezvada-pf/static/media/BannerImage2.90264d25.jpg"
+  },
+  {
+    "revision": "fc75389206fa60d9c305f967281e229c",
+    "url": "/lukezvada-pf/static/media/Decks.fc753892.jpeg"
+  },
+  {
+    "revision": "8c02a7493936f1447d925d9c0e424138",
+    "url": "/lukezvada-pf/static/media/Django.8c02a749.png"
+  },
+  {
+    "revision": "ee4ad5ac70578f2190572d8d13dff2ce",
+    "url": "/lukezvada-pf/static/media/LZ.ee4ad5ac.jpg"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/lukezvada-pf/static/media/MUIlogo.d2b1e190.png"
+  },
+  {
+    "revision": "f697ba0c9244a5495766983a27636ff4",
+    "url": "/lukezvada-pf/static/media/SqLite.f697ba0c.png"
+  },
+  {
+    "revision": "6a62d2b8f0d02cc7e262738f0fe759af",
+    "url": "/lukezvada-pf/static/media/bootstrap.6a62d2b8.png"
+  },
+  {
+    "revision": "04debe24811da54a2abddc709d41f5fd",
+    "url": "/lukezvada-pf/static/media/figma.04debe24.png"
+  },
+  {
+    "revision": "50a67f1cbe0f46b4cd999a8d083155bc",
+    "url": "/lukezvada-pf/static/media/jslogo.50a67f1c.png"
+  },
+  {
+    "revision": "a9ac36847782861e4708eec943f6e0fb",
+    "url": "/lukezvada-pf/static/media/json.a9ac3684.png"
+  },
+  {
+    "revision": "2364293f8c147a20747c2a2fe3ab6604",
+    "url": "/lukezvada-pf/static/media/pythonlogo.2364293f.jpg"
+  },
+  {
+    "revision": "ab2f9e05cafe9d9adf2cd9bc864681c8",
+    "url": "/lukezvada-pf/static/media/react-logo.ab2f9e05.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lukezvada-pf/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lukezvada-pf/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lukezvada-pf/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lukezvada-pf/static/media/slick.f97e3bbf.svg"
+  }
+]);
