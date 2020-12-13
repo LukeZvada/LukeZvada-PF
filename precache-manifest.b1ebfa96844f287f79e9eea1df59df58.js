39a10228e377268d6f97ec6a0ac909fa8efade5a
LukeZvada <zvadamusic@gmail.com>
2020-12-13 13:29:06 -0500

    Updates

---
+++ precache-manifest.b1ebfa96844f287f79e9eea1df59df58.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c72e01edb60e583a46d430bf4b29b4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9bc4d8e57d0a8ec8ed0",
+    "url": "/static/css/2.47dce79d.chunk.css"
+  },
+  {
+    "revision": "85f325fa1f988320ae92",
+    "url": "/static/css/main.69e39664.chunk.css"
+  },
+  {
+    "revision": "e9bc4d8e57d0a8ec8ed0",
+    "url": "/static/js/2.a94cc1c1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a94cc1c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f325fa1f988320ae92",
+    "url": "/static/js/main.1b57ade7.chunk.js"
+  },
+  {
+    "revision": "7932af8c3f9b7a267f4a",
+    "url": "/static/js/runtime-main.fd24249f.js"
+  },
+  {
+    "revision": "dddf28fe8c3cc12111c8068f1d1db134",
+    "url": "/static/media/AXDlogo.dddf28fe.png"
+  },
+  {
+    "revision": "90264d25bdc8c70d30073fd73fe3b042",
+    "url": "/static/media/BannerImage2.90264d25.jpg"
+  },
+  {
+    "revision": "fc75389206fa60d9c305f967281e229c",
+    "url": "/static/media/Decks.fc753892.jpeg"
+  },
+  {
+    "revision": "8c02a7493936f1447d925d9c0e424138",
+    "url": "/static/media/Django.8c02a749.png"
+  },
+  {
+    "revision": "ee4ad5ac70578f2190572d8d13dff2ce",
+    "url": "/static/media/LZ.ee4ad5ac.jpg"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/static/media/MUIlogo.d2b1e190.png"
+  },
+  {
+    "revision": "f697ba0c9244a5495766983a27636ff4",
+    "url": "/static/media/SqLite.f697ba0c.png"
+  },
+  {
+    "revision": "6a62d2b8f0d02cc7e262738f0fe759af",
+    "url": "/static/media/bootstrap.6a62d2b8.png"
+  },
+  {
+    "revision": "04debe24811da54a2abddc709d41f5fd",
+    "url": "/static/media/figma.04debe24.png"
+  },
+  {
+    "revision": "50a67f1cbe0f46b4cd999a8d083155bc",
+    "url": "/static/media/jslogo.50a67f1c.png"
+  },
+  {
+    "revision": "a9ac36847782861e4708eec943f6e0fb",
+    "url": "/static/media/json.a9ac3684.png"
+  },
+  {
+    "revision": "2364293f8c147a20747c2a2fe3ab6604",
+    "url": "/static/media/pythonlogo.2364293f.jpg"
+  },
+  {
+    "revision": "ab2f9e05cafe9d9adf2cd9bc864681c8",
+    "url": "/static/media/react-logo.ab2f9e05.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/LukeZvada-PF/precache-manifest.86a7566bd46b53437554fe200b8fa943.js"
+  "/precache-manifest.b1ebfa96844f287f79e9eea1df59df58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LukeZvada-PF/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
